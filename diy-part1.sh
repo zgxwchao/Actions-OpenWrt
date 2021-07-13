ffbe092d3e1096dff19e17859bb97f3e85b02551
zgxwchao <62455260+zgxwchao@users.noreply.github.com>
2021-07-13 12:23:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/281677160/openwrt-package package/danshui
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+#git clone https://github.com/281677160/openwrt-package package/danshui
+#git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0 package/k2p-openwrt-mt7615_5.0.2.0
 git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
